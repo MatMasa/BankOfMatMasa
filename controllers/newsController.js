af01c2a1071d280ec4e998b206eab20683926b24
MatMasa <matti.wallenius@edu.turkuamk.fi>
2021-04-26 07:07:12 +0300

    Fixit

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -32,7 +32,7 @@ module.exports = {
         try {
             const allNews = await newsArticle.find()
             res.render('newsManagement', {
-                allNews: allNews
+                allNews
             });
         } catch (err) {
             res.render('error', {
